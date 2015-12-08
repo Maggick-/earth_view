1e4b6655c28638ae1a2b29dbece222140074da1f
Maurice Schleußinger <Maurice.Schleussinger@uni-duesseldorf.de>
2015-12-08 19:02:54 +0100

    Avoid UnicodeEncodeError by using unicode instead of strings

--- earth-view.widget/load_image.py
+++ earth-view.widget/load_image.py
@@ -4,12 +4,10 @@ from subprocess import call
 
 with open('earth-view.widget/response.json', 'rb') as response:
     res = json.load(response)
-photo_url = res['photoUrl']
-next_api = 'https://earthview.withgoogle.com' + res['nextApi']
+photo_url = unicode(res['photoUrl'])
+next_api = unicode('https://earthview.withgoogle.com' + res['nextApi'])
 
-print photo_url
-print next_api
 call(["curl", "-so",
-      "earth-view.widget/images/img.jpg", "{0}".format(photo_url)])
+      u"earth-view.widget/images/img.jpg", photo_url])
 call(["curl", "-so",
-      "earth-view.widget/response.json", "{0}".format(next_api)])
+      u"earth-view.widget/response.json", next_api])
